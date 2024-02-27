47cc5c2909bafb00724fd6e888d29a85c85dd8b5
itishree2 <nayakitishree6262@gmail.com>
2024-02-27 17:02:52 +0530

    initial.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template fo
+for(int num1=1;num1<=10;num1++)
+{
+    Console.WriteLine(num1);
+    Thread.Sleep(1000);
+
+}
+Console.WriteLine("10 to 100");
+for(int num2=10;num2<=100;num2++)
+{
+    Console.WriteLine(num2);
+    Thread.Sleep(1000);
+}
